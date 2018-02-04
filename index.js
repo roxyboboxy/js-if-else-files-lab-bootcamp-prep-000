873409b26de2638616051cfd241cef92bdcc8288
roxanne horwath <roxyboboxy@gmail.com>
2018-02-04 03:12:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var name = "Joe"
 var height = "74"
 
+var name = "Susan"
+var height = "70"
+
+if (name === "Joe") {
+  console.log ("The name is correct")
+} 
+if (height === "74") {
+  console.log ("The height is correct")
+}
+
+if (name === "Susan") {
+  console.log ("The name is correct")
+} 
+if (height === "70") {
+  console.log ("The height is correct")
+}
 
 // Don't worry about this
 module.exports = { name, height
